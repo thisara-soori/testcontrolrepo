f8135a763e1a402d886da7200ebb6b16931df88b
thisara-soori <thisarasooriyarachchi@gmail.com>
2017-12-11 04:58:10 +0530

    Create service.pp

---
+++ modules/testing/manifests/service.pp
@@ -0,0 +1,13 @@
+class testing::service (
+String $srvicename = "httpd",
+) {
+  service { "${srvicename}":
+    ensure => running,
+    enable => true,
+    hasrestart => true,
+    hasstatus  => true,
+    # pattern => 'name',
+    require => Class ['testing::install'],
+  }
+}
+}
