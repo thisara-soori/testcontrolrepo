177bd7329d1e5c74ece3aaed22540b6430c0eedf
thisara-soori <thisarasooriyarachchi@gmail.com>
2017-12-11 06:07:30 +0530

    Update configuration.pp

--- modules/testing/manifests/configuration.pp
+++ modules/testing/manifests/configuration.pp
@@ -5,7 +5,7 @@ Integer  $httpd_port = 90,
   file { '/etc/httpd/conf/httpd.conf':
     ensure => file,
     mode => '0644',
-    content => epp('testing/httpd.conf.epp',90),
+    content => epp('testing/httpd.conf.epp',{'httpd_port' => 90}),
     #content => template('httpd/httpd.conf.erb'),
     #require => ['httpd::install'],
     notify => Class['testing::service'],
