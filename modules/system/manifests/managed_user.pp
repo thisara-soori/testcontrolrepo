defdba5ebe3cbbf3d062decea90235e5da74ad31
thisara-soori <thisarasooriyarachchi@gmail.com>
2017-12-13 05:35:46 +0530

    Update managed_user.pp

--- modules/system/manifests/managed_user.pp
+++ modules/system/manifests/managed_user.pp
@@ -1,18 +1,23 @@
 define system::managed_user (
-	$homedir = undef
-	$password = '$1$eEFWtZca$TI5YVc1w0jkDDzkTd8fsr1',
-) 
- 
-user { $title:
-ensure => present,
-password => $password,
-managehome => true,
-}
-if $kernel == 'Linux' {
-file { "${homedir}/.bashrc":
-ensure => file,
-owner => $title,
-group => $title,
-mode => '0644',
-source => 'puppet:///modules/system/bashrc'
+	$password  = '$1$8PXwL66U$lRQkeCgMWyO5/weIkQ7WD0',
+	$home_directory = undef
+
+) {
+	user { "${title}":
+		ensure     => present,
+		password   => "${password}",
+		managehome => true,
+		
+	}
+	
+	if $facts['fact_name'] == 'Linux' {
+		file { "${home_directory}/.bashrc":
+			ensure => file,
+			owner  => "${title}",
+			group  => "${title}",
+			mode   => '0644',
+			source => 'puppet:///modules/system/bashrc'
+		}
+	}
+
 }
