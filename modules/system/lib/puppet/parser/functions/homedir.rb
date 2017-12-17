30dd03c4461dd63f09ef2eceea995edfba84b150
thisara-soori <thisarasooriyarachchi@gmail.com>
2017-12-17 19:39:14 +0530

    Update homedir.rb

--- modules/system/lib/puppet/parser/functions/homedir.rb
+++ modules/system/lib/puppet/parser/functions/homedir.rb
@@ -1,2 +1,18 @@
 # Write a simple function called homedir that accepts a single string argument.
 # It should return the conventional Linux home directory based on a username
+module Puppet::Parser::Functions
+        newfunction(:homedir,
+        :type => :rvalue,
+        :arity => 1
+        ) do |args|
+        raise ArgumentError, 'Expects a string' unless args.first.is_a? String
+        username = args[0]
+          case user
+            when 'root'
+            return '/root'
+            else
+            return "/home/#{user}"
+          end
+          
+        end
+end
