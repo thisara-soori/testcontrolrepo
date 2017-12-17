1c3aec3dd2650b6858b542e4622dedd2b8447bfd
thisara-soori <thisarasooriyarachchi@gmail.com>
2017-12-17 06:45:21 +0530

    Update classroom.pp

--- modules/system/manifests/classroom.pp
+++ modules/system/manifests/classroom.pp
@@ -1,9 +1,15 @@
 class system::classroom {
   # export a virtual host resource for yourself
-
+  @@host { "$facts['fqdn']":
+    ensure => 'present',
+    host_aliases => [$::hostname],
+    ip => $::ipaddress,
+    tag => 'classroom',
+  }
+ 
 
   # collect all resources from the database (including your own)
   # enforce only those tagged with `classroom`.
-
+  Host <<| tag == 'classroom' |>>
 
 }
