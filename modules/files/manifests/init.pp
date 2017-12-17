e250d4832a27f40c193d6e27a3d82445b59b9bbf
thisara-soori <thisarasooriyarachchi@gmail.com>
2017-12-17 22:11:25 +0530

    Update init.pp

--- modules/files/manifests/init.pp
+++ modules/files/manifests/init.pp
@@ -13,6 +13,12 @@ class files {
     line   => 'root',
   }
   # Add a rule to cron.deny to deny jobs by default
+  
+  file_line { 'disable others cron jobs':
+    ensure => present,
+    path   => '/etc/cron.deny',
+    line   => 'root',
+  }
 
 
   # What concat resource is needed for this fragment to work?
@@ -23,6 +29,12 @@ class files {
   }
 
   # Add a few fragments to be appended to /etc/motd
+  
+  concat::fragment { 'sample motd message': 
+    target  => '/etc/motd',
+    order   => '50',
+    content => "This is a sample motd message\n",
+  }
 
 
 }
