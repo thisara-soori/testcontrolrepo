449ee34978ed6c292fc94d7665be681b7b5426db
thisara-soori <thisarasooriyarachchi@gmail.com>
2017-12-17 09:06:17 +0530

    Update default_realm.rb

--- modules/kerberos/lib/facter/default_realm.rb
+++ modules/kerberos/lib/facter/default_realm.rb
@@ -1,2 +1,9 @@
 # Write a fact that returns the value of running the command
 # "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
+
+
+Facter.add('default_realm') do
+  setcode do
+    Facter::Core::Execution.exec('/usr/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf')
+  end
+end
