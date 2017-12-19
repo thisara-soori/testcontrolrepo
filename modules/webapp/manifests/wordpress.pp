411aeee03a0a55a00a14b05aa19c93e51245417b
thisara-soori <thisarasooriyarachchi@gmail.com>
2017-12-19 05:37:07 +0530

    Update wordpress.pp

--- modules/webapp/manifests/wordpress.pp
+++ modules/webapp/manifests/wordpress.pp
@@ -1,6 +1,10 @@
 class webapp::wordpress {
  
-  include ::wordpress
+  #include ::wordpress
+  class { 'wordpress'
+    install_url => "http://10.0.2.15",
+  }
+  
   class {'webapp':
     docroot => '/opt/wordpress',
     app_name => 'wordpress',
