08d55f59264255fb6c5a6c61cf8bfd5d05b16a93
thisara-soori <thisarasooriyarachchi@gmail.com>
2017-12-20 16:33:02 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,5 +5,7 @@ notify { "Root's home directory is ${homedir('root')}": }
 notify { "Test's home directory is ${homedir('test')}": }
 }
 node default {
-notify { "Node ${fqdn} has no node definition": }
+#notify { "Node ${fqdn} has no node definition": }
+$message = lookup('message')
+notify { $message: }
 }
